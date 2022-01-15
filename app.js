b96fc0aa216480d37c406942f73ecbd1c654c7a7
YaroslavHodos <h0543510343@gmail.com>
2022-01-15 16:40:44 +0200

    hw#11

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-
-//function encode(num, base) {
-    //base from 2 to 10
-//     let res =""; 
-//     do {
-//         const digit = Math.trunc(num % base);
-//         const symb = getSymbol(digit);
-//         res = symb + res;
-//         num = Math.trunc(num / base);
-//     } while(num >= 1);
-//     return res;
-// }
-// function getSymbol(digit) {
-    //base from 2 to 10
-//    return "" + digit;// it will work only for base <= 10
-}
-// console.log(encode(10,2));
-// write function
-
 function encode1(num, codingString) 
 {
     //codingString - any string with not repeated symbols

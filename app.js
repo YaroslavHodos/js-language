41fa1d1023e9224e931b4717da4718628f466870
YaroslavHodos <h0543510343@gmail.com>
2022-01-20 18:39:20 +0200

    HW#12

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-function encode(num, codingString)
-{
-    let flRepeated = false;
-    for (let i = 0; i < codingString.length; i++)
-    {
-        if (codingString.lastIndexOf(codingString[i]) != i)
-        {
-            console.log(`coding string has repeated symbol ${codingString[i]}`)
-            flRepeated = true;
-        }
+console.log("HW#12");
+const comparator = (n1,n2) => {
+    let res = n1 % 2 - n2 % 2;
+    if (res == 0) {
+        res = n1 % 2 == 0 ? n1 - n2 : n2 - n1;
     }
-    if (!flRepeated)
-    {
-        let res = "";
-        const base = codingString.length;
-        do
-        {
-            const digit = Math.trunc(num % base);
-            const digitSymb = codingString[digit];
-            res = digitSymb + res;
-            num = Math.trunc(num / base)
+    return res;
+}
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+arHw.sort(comparator);
+console.log(arHw);
+
+const matrix =  [[1,2],[3,4],[5,6]];
+function matrixTransp(matrix) {
+    const res = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        res.push([]);
+        for (let j = o; j < matrix.length; j++) {
+            res[i].push(matrix[j][i]);
         }
-        while(num != 0);
-        return res;
     }
 }
-console.log(`encode(5, '.-') = ${encode(5, '.-')}`);
+displsyMatrix(matrix);
+
+function displsyMatrix(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        let row = '';
+        console.log('')
+        for(let j = 0; j <matrix[i].length; j++) {
+            row = row + matrix[i][j] + '   ';
+        }
+        console.log(row);
+    }
+}

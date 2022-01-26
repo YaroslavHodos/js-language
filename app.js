8a25b0eba656986b469096246aab949ef0db4a2a
YaroslavHodos <h0543510343@gmail.com>
2022-01-26 18:29:50 +0200

    CW#14

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const ar2 = [13, 17, 20, 23, 2, 40];
-const arEvenOdd = ar2.filter((n, i, a) => a.length % 2 == 0 ? n % 2 == 0 : n % 2 == 1);
-console.log(arEvenOdd);
+const person1 = {id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
+const person2 = {id: 123, name: 'Moshe', address: {city: 'Lod', street: 'Sokolov'}};
 
-/*
+console.log(`'123' == 123 is ${'123' == 123}`);
+console.log(`'123' === 123 is ${'123' === 123}`);
+
+console.log(`person1 == person2 is ${person1 == person2}`);
+console.log(`JSON.stringify(person1) == JSON.stringify(person2) is ${JSON.stringify(person1) == JSON.stringify(person2)}`);
+console.log(JSON.stringify(person1));
+console.log(`name of person1 is ${person1.name}`);
+console.log(`pers1 live in sity ${person1.addres.city}`);
+
+Object.keys(person1).forEach(k => console.log(k));// array of the object keys
+console.log("");
+
+Object.values(person1).forEach(v => console.log(v));// array of the object values
+console.log("");
+
+Object.entries(person1).forEach(c => console.log(c));// array of arrays - [key, value]
+console.log("");
+console.log(Object.entries(person1));// array of arrays - [key, value]
+
+function createAddress(city, street) {
+  // return {city: city, street: street} = {city, street}
+  return {city, street};
+}
+function createPerson(id, name, address) {
+  return {id, name, address};
+}
+
+const persons = [
+  createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
+  createPerson(124, "Olya", createAddress("Rehovot", "Plaut")),
+  createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
+]
+/**
 HW#14
-  1.
-write myFilter(array, callback) based on myForEach
-callback - function with three possible parameters: current element, current index, referece to array
-  2.
-write myReduce(array, callback, initialResult) based on myForEach
-callback - function with three possible parameters: current element, current index, referece to array
 
-*/
-
-let res = ar2.reduce((res, cur) => res + cur, 0);
-console.log(`res = ${res}`);
-const max = ar2.reduce((max, cur) => cur > max ? cur : max, ar2[0]);
-console.log(`max = ${max}`);
-res = ar2.reduce((res, cur) => res + cur);
-console.log(`res = ${res}`);// if the user call does't contain a second argument, then the first element of the array will be
-// as initial res (in this case iterating begins from the second element of hte array)
-
-/*
-
+3.
+applaying methods of arrays you shold find the persons living in Rehovot and display them out
+  4.
+applaying methods of arrays you shold find the person don't liv in Rehovot and move the at begining of the array persons
  */

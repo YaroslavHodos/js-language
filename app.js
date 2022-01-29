0a3f24a1ec84d69044d969ab51f0ba59d6f69ae6
YaroslavHodos <h0543510343@gmail.com>
2022-01-29 14:49:00 +0200

    CW#15

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
-function myForEach(ar, cbFunction) {
-    for (let i = 0; i < ar.length; i++) {
-        cbFunction(ar[i], i, ar);
+// input : ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"]
+/*
+output : lmn -> 3
+         a -> 2
+         d -> 2
+         bc -> 1
+*/
+function displayOccurences(array) {
+    //creating object with key - unique of array (string as an element of array)
+    //                value - occurences count
+    //difference between obj.a and obj["a"]
+    const res = {};
+    for (let i = 0; i < array.length; i++) {
+        if (res[array[i]] === undefined) {
+            //string as content of array[i] occures first time
+            res[array[i]] = 1;
+        } 
+        else {
+            res[array[i]] = res[array[i]] + 1;
+        }
     }
-}
+    //console.log(res); -> intermediate debug log
+    Object.entries(res).sort((e1, e2) => {
+        const res = e2[1] - e1[1];
+        return res === 0 ? e1[0].localeCompare(e2[0]) : res;
+    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
 
-//1.
-function myFilter (arr, cb) {
-    const newAr = [];
-    myForEach(arr, (n, i, a) => cb (n, i, a) ? newAr.push(n) : 0);
-    return newAr;
 }
+const arString =  ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
+displayOccurences(arString);
 
-//2.
-function myReduce(array, cb, initRes) {
-    let res;
-    if (initRes == undefined) {
-        initRes = array[0];
-        array = array.slice(1);
-    }
-    else {
-        res = initRes;
-    }
-    myForEach(array, (n, i, a) => res = cb(res, n, i, a));
-    return res;
-}
-const ar20 = [13, 17, 20, 23, 2, 40, 15];
-const arEvOdd = myFilter(ar20, (n, i, a) => a.length %2 == 0 ? n %2 == 0: n %2 == 1);
-console.log(arEvOdd);
-const result = myReduce(ar20, (res, cur) => res + cur, 0);
-console.log(result);
+/*HW #15
+  1.
+refactoring of function displayOccurences
+lines 92 -99 should be a separated function
+that separated function should apply methods like reduce
+  2.
+write useful function countBy(array, collbackFun) that returns object 
+with keys as grouping criteria and values as the occurrence counts
+keys should be sorted (optional)
+where array - any array, collbackFun - function returning grouping criteria
+examples:
+1.
+const arr = [6.4, 7.3, 6.5, 6.9];
+const statistics = countBy(arr, element => Math.flour(element))
+result: statistics -> {"6": 3, "7": 1}
+2.
+const arr = ["abcd", "lmnr", "ab", "dddd"];
+const statistics = countBy(arr, element => element.length)
+rwsult: statistics -> {"4": 3, "2": 1}
+3.
+const arr = [{age: 25, id: 123, name: "Vasya"},{age: 50, id: 123, name: "Vasya"},
+{age: 25, id: 123, name: "Vasya"},{age: 70, id: 123, name: "Vasya"}];
+const statistics = countBy(arr, element.age)
+result statistics -> {"25": 2, "50": 1, "70": 1}
+ */
